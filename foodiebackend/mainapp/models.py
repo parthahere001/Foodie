b0e86a20fa014f7ac05de3d71f764951301b9ed8
parthahere001 <parthahere001.com>
2023-05-20 15:20:09 +0530

    [ADD] Dish Model

--- foodiebackend/mainapp/models.py
+++ foodiebackend/mainapp/models.py
@@ -4,3 +4,12 @@ class hotelModel(models.Model):
     name = models.CharField(max_length=255)
     location = models.CharField(max_length=3000)
     rating = models.IntegerField()
+
+class dishModel(models.Model):
+    name = models.CharField(max_length=200)
+    pic = models.ImageField(upload_to='foodImages/')
+    type = models.CharField(max_length=50)
+    quantity = models.IntegerField()
+    description = models.CharField(max_length=500)
+
+    
